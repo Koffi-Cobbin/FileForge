2c53a270b09df4a4331a58a7d78017ccca27bc8b
Koffi-Cobbin <ocupualorelijah@gmail.com>
2026-04-27 18:18:57 +0000

    added collectstatic

--- fileforge/urls.py
+++ fileforge/urls.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def root(_request):
@@ -29,3 +31,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("storage.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
